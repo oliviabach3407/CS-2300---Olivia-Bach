bccf68d3ca32f50264d3b7ad1b5848eb6e184cce
oliviabach3407 <111521814+oliviabach3407@users.noreply.github.com>
2023-03-07 19:05:58 -0700

    Second Elimination figured out

--- Program 2/ProgrammingAssignment2.Pt2.c
+++ Program 2/ProgrammingAssignment2.Pt2.c
@@ -17,6 +17,7 @@ void fgetsRemoveNewLine(char* buff);
 void getMatrix(double newMatrix[4]);
 bool determinate(double testMatrix[4]);
 void firstElimination(double firstMatrix[4], double firstStep[4], double identity[4], double newIdentity[4]);
+void secondElimination(double original[4], double incomingMatrix[4], double newMatrix[4], double incomingIdentity[4], double newIdentity[4]);
 
 int main(void) {
 
@@ -27,6 +28,10 @@ int main(void) {
 	double firstMatrix[4] = { 0 };
 	double identityMatrix[4] = {1, 0, 0, 1};  //in order of a11, a21, a12, a22
 	double newIdentity[4] = { 0 };
+
+	double secondMatrix[4] = { 0 };
+	double newIdentity2[4] = { 0 }; 
+
 	bool inverseOrNo = true;
 
 	getMatrix(matrix);
@@ -38,11 +43,11 @@ int main(void) {
 	if (inverseOrNo) {
 		//calculate inverse with gauss elimination
 
-		if (matrix[1] != 0) {
+		if (matrix[1] != 0) { //if that spot isn't already a 0
 			firstElimination(matrix, firstMatrix, identityMatrix, newIdentity);
 
-			puts("\nRESULTS OF FIRST STEP:");
-			puts("\n-------------------------\n\n");
+			puts("\n\nRESULTS OF FIRST STEP:");
+			puts("\n-------------------------");
 
 			puts("\nOriginal matrix multiplied with S:");
 
@@ -53,6 +58,23 @@ int main(void) {
 
 			printf("\n| %5.2lf   %5.2lf |", newIdentity[0], newIdentity[2]);
 			printf("\n| %5.2lf   %5.2lf |", newIdentity[1], newIdentity[3]);
+		}
+
+		if (matrix[2] != 0) {
+			secondElimination(matrix, firstMatrix, secondMatrix, newIdentity, newIdentity2);
+
+			puts("\n\nRESULTS OF SECOND STEP:");
+			puts("\n-------------------------");
+
+			puts("\nPrevious matrix multiplied with S:");
+
+			printf("\n| %5.2lf   %5.2lf |", secondMatrix[0], secondMatrix[2]);
+			printf("\n| %5.2lf   %5.2lf |", secondMatrix[1], secondMatrix[3]);
+
+			puts("\n\n\nPrevious identity matrix multiplied with S:");
+
+			printf("\n| %5.2lf   %5.2lf |", newIdentity2[0], newIdentity2[2]);
+			printf("\n| %5.2lf   %5.2lf |", newIdentity2[1], newIdentity2[3]);
 		}
 
 
@@ -150,12 +172,9 @@ bool determinate(double testMatrix[4]) {
 }
 
 void firstElimination(double firstMatrix[4], double firstStep[4], double identity[4], double newIdentity[4]) {
-	
-	double total1 = 0;
-	double total2 = 0;
 
 	double sOne11 = 1;
-	double sOne21 =  (-(firstMatrix[1])) / firstMatrix[0];
+	double sOne21 = (-(firstMatrix[1])) / firstMatrix[0];
 	double sOne12 = 0;
 	double sOne22 = 1;
 
@@ -172,5 +191,28 @@ void firstElimination(double firstMatrix[4], double firstStep[4], double identit
 
 	newIdentity[2] = (sOne11 * identity[2]) + (sOne12 * identity[3]);  //a12 for newIdentity
 	newIdentity[3] = (sOne21 * identity[2]) + (sOne22 * identity[3]);  //a22 for newIdentity
+
+}
+
+void secondElimination(double original[4], double incomingMatrix[4], double newMatrix[4], double incomingIdentity[4], double newIdentity[4]) {
+
+	double sOne11 = 1;
+	double sOne21 = 0;
+	double sOne12 = original[0] / original[1];
+	double sOne22 = 1;
+
+	//multiplying incoming matrix and incoming identity with new S matrix
+
+	newMatrix[0] = (sOne11 * incomingMatrix[0]) + (sOne12 * incomingMatrix[1]);  //a11 for firstStep
+	newMatrix[1] = (sOne21 * incomingMatrix[0]) + (sOne22 * incomingMatrix[1]);  //a21 for firstStep
+
+	newMatrix[2] = (sOne11 * incomingMatrix[2]) + (sOne12 * incomingMatrix[3]);  //a12 for firstStep
+	newMatrix[3] = (sOne21 * incomingMatrix[2]) + (sOne22 * incomingMatrix[3]);  //a22 for firstStep
+
+	newIdentity[0] = (sOne11 * incomingIdentity[0]) + (sOne12 * incomingIdentity[1]);  //a11 for newIdentity
+	newIdentity[1] = (sOne21 * incomingIdentity[0]) + (sOne22 * incomingIdentity[1]);  //a21 for newIdentity
+
+	newIdentity[2] = (sOne11 * incomingIdentity[2]) + (sOne12 * incomingIdentity[3]);  //a12 for newIdentity
+	newIdentity[3] = (sOne21 * incomingIdentity[2]) + (sOne22 * incomingIdentity[3]);  //a22 for newIdentity
 
 }
