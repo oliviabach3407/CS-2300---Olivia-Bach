382e88aff50a8afc0a8b4f702904c0b9b8d90c9e
oliviabach3407 <111521814+oliviabach3407@users.noreply.github.com>
2023-03-07 19:44:24 -0700

    Preemptive Save

--- Program 2/ProgrammingAssignment2.Pt2.c
+++ Program 2/ProgrammingAssignment2.Pt2.c
@@ -16,8 +16,10 @@
 void fgetsRemoveNewLine(char* buff);
 void getMatrix(double newMatrix[4]);
 bool determinate(double testMatrix[4]);
-void firstElimination(double firstMatrix[4], double firstStep[4], double identity[4], double newIdentity[4]);
-void secondElimination(double original[4], double incomingMatrix[4], double newMatrix[4], double incomingIdentity[4], double newIdentity[4]);
+void firstElimination(double firstMatrix[4], double firstStep[4], double identity[4], double newIdentity[4], double sOne[4]);
+void secondElimination(double original[4], double incomingMatrix[4], double newMatrix[4], double incomingIdentity[4], double newIdentity[4], double sTwo[4]);
+void thirdElimination(double original[4], double incomingMatrix[4], double newMatrix[4], double incomingIdentity[4], double newIdentity[4], double sThree[4]);
+void calculateInverse(double sOne[4], double sTwo[4], double sThree[4], double completeInverse[4]);
 
 int main(void) {
 
@@ -32,6 +34,15 @@ int main(void) {
 	double secondMatrix[4] = { 0 };
 	double newIdentity2[4] = { 0 }; 
 
+	double thirdMatrix[4] = { 0 };
+	double newIdentity3[4] = { 0 };
+
+	double finalInverse[4] = { 0 };
+
+	double s1[4] = { 0 };
+	double s2[4] = { 0 };
+	double s3[4] = { 0 };
+
 	bool inverseOrNo = true;
 
 	getMatrix(matrix);
@@ -44,7 +55,7 @@ int main(void) {
 		//calculate inverse with gauss elimination
 
 		if (matrix[1] != 0) { //if that spot isn't already a 0
-			firstElimination(matrix, firstMatrix, identityMatrix, newIdentity);
+			firstElimination(matrix, firstMatrix, identityMatrix, newIdentity, s1);
 
 			puts("\n\nRESULTS OF FIRST STEP:");
 			puts("\n-------------------------");
@@ -61,7 +72,7 @@ int main(void) {
 		}
 
 		if (matrix[2] != 0) {
-			secondElimination(matrix, firstMatrix, secondMatrix, newIdentity, newIdentity2);
+			secondElimination(matrix, firstMatrix, secondMatrix, newIdentity, newIdentity2, s2);
 
 			puts("\n\nRESULTS OF SECOND STEP:");
 			puts("\n-------------------------");
@@ -77,6 +88,31 @@ int main(void) {
 			printf("\n| %5.2lf   %5.2lf |", newIdentity2[1], newIdentity2[3]);
 		}
 
+		if (matrix[0] != 1 && matrix[3] != 1) {
+			thirdElimination(matrix, secondMatrix, thirdMatrix, newIdentity2, newIdentity3, s3);
+
+			puts("\n\nRESULTS OF THIRD STEP:");
+			puts("\n-------------------------");
+
+			puts("\nPrevious matrix multiplied with S:");
+
+			printf("\n| %5.2lf   %5.2lf |", thirdMatrix[0], thirdMatrix[2]);
+			printf("\n| %5.2lf   %5.2lf |", thirdMatrix[1], thirdMatrix[3]);
+
+			puts("\n\n\nPrevious identity matrix multiplied with S:");
+
+			printf("\n| %5.2lf   %5.2lf |", newIdentity3[0], newIdentity3[2]);
+			printf("\n| %5.2lf   %5.2lf |", newIdentity3[1], newIdentity3[3]);
+		}
+
+		calculateInverse(s1, s2, s3, finalInverse);
+
+		puts("\n\n-------------------------");
+		puts("\nFINAL INVERSE:");
+		puts("\n-------------------------");
+
+		printf("\n| %5.2lf   %5.2lf |", finalInverse[0], finalInverse[2]);
+		printf("\n| %5.2lf   %5.2lf |", finalInverse[1], finalInverse[3]);
 
 	}
 	else {
@@ -171,12 +207,17 @@ bool determinate(double testMatrix[4]) {
 
 }
 
-void firstElimination(double firstMatrix[4], double firstStep[4], double identity[4], double newIdentity[4]) {
+void firstElimination(double firstMatrix[4], double firstStep[4], double identity[4], double newIdentity[4], double sOne[4]) {
 
 	double sOne11 = 1;
 	double sOne21 = (-(firstMatrix[1])) / firstMatrix[0];
 	double sOne12 = 0;
 	double sOne22 = 1;
+
+	sOne[0] = sOne11;
+	sOne[1] = sOne21;
+	sOne[2] = sOne12;
+	sOne[3] = sOne22;
 
 	//multiplying og matrix and identity matrix with S matrix 
 
@@ -194,12 +235,17 @@ void firstElimination(double firstMatrix[4], double firstStep[4], double identit
 
 }
 
-void secondElimination(double original[4], double incomingMatrix[4], double newMatrix[4], double incomingIdentity[4], double newIdentity[4]) {
+void secondElimination(double original[4], double incomingMatrix[4], double newMatrix[4], double incomingIdentity[4], double newIdentity[4], double sTwo[4]) {
 
 	double sOne11 = 1;
 	double sOne21 = 0;
 	double sOne12 = original[0] / original[1];
 	double sOne22 = 1;
+
+	sTwo[0] = sOne11;
+	sTwo[1] = sOne21;
+	sTwo[2] = sOne12;
+	sTwo[3] = sOne22;
 
 	//multiplying incoming matrix and incoming identity with new S matrix
 
@@ -214,5 +260,57 @@ void secondElimination(double original[4], double incomingMatrix[4], double newM
 
 	newIdentity[2] = (sOne11 * incomingIdentity[2]) + (sOne12 * incomingIdentity[3]);  //a12 for newIdentity
 	newIdentity[3] = (sOne21 * incomingIdentity[2]) + (sOne22 * incomingIdentity[3]);  //a22 for newIdentity
+
+}
+
+void thirdElimination(double original[4], double incomingMatrix[4], double newMatrix[4], double incomingIdentity[4], double newIdentity[4], double sThree[4]) {
+
+	double sOne11 = 1/incomingMatrix[0];
+	double sOne21 = 0;
+	double sOne12 = 0;
+	double sOne22 = 1/incomingMatrix[3];
+
+	sThree[0] = sOne11;
+	sThree[1] = sOne21;
+	sThree[2] = sOne12;
+	sThree[3] = sOne22;
+
+	//multiplying incoming matrix and incoming identity with new S matrix
+
+	newMatrix[0] = (sOne11 * incomingMatrix[0]) + (sOne12 * incomingMatrix[1]);  //a11 for firstStep
+	newMatrix[1] = (sOne21 * incomingMatrix[0]) + (sOne22 * incomingMatrix[1]);  //a21 for firstStep
+
+	newMatrix[2] = (sOne11 * incomingMatrix[2]) + (sOne12 * incomingMatrix[3]);  //a12 for firstStep
+	newMatrix[3] = (sOne21 * incomingMatrix[2]) + (sOne22 * incomingMatrix[3]);  //a22 for firstStep
+
+	newIdentity[0] = (sOne11 * incomingIdentity[0]) + (sOne12 * incomingIdentity[1]);  //a11 for newIdentity
+	newIdentity[1] = (sOne21 * incomingIdentity[0]) + (sOne22 * incomingIdentity[1]);  //a21 for newIdentity
+
+	newIdentity[2] = (sOne11 * incomingIdentity[2]) + (sOne12 * incomingIdentity[3]);  //a12 for newIdentity
+	newIdentity[3] = (sOne21 * incomingIdentity[2]) + (sOne22 * incomingIdentity[3]);  //a22 for newIdentity
+
+}
+
+void calculateInverse(double sOne[4], double sTwo[4], double sThree[4], double completeInverse[4]) {
+	
+	//multiply all the s's together --> s3 * s2 * s1
+
+	double tempMatrix[4] = { 0 };
+
+	//first multiplication (s3 * s2)
+
+	tempMatrix[0] = (sThree[0] * sTwo[0]) + (sThree[2] * sTwo[1]);
+	tempMatrix[2] = (sThree[0] * sTwo[2]) + (sThree[2] * sTwo[3]); 
+
+	tempMatrix[1] = (sThree[1] * sTwo[0]) + (sThree[3] * sTwo[1]);
+	tempMatrix[3] = (sThree[1] * sTwo[2]) + (sThree[3] * sTwo[3]);
+
+	//second multiplication ((s3*s2) * s1)
+
+	completeInverse[0] = (tempMatrix[0] * sOne[0]) + (tempMatrix[2] * sOne[1]);
+	completeInverse[2] = (tempMatrix[0] * sOne[2]) + (tempMatrix[2] * sOne[3]);
+
+	completeInverse[1] = (tempMatrix[1] * sOne[0]) + (tempMatrix[3] * sOne[1]);
+	completeInverse[3] = (tempMatrix[1] * sOne[2]) + (tempMatrix[3] * sOne[3]);
 
 }
