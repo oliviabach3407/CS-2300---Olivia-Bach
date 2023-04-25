86ccb3d82ad2a9585e47640732eb33096a6851ab
oliviabach3407 <111521814+oliviabach3407@users.noreply.github.com>
2023-04-25 17:41:32 -0600

    FIXED FILE INPUT

--- Program 4/ProgrammingAssignment4.Pt1.c
+++ Program 4/ProgrammingAssignment4.Pt1.c
@@ -21,20 +21,22 @@ sub-parts where each line includes three projected image points, similarly to th
 #include <math.h>
 
 #define LENGTH_FILE_PATH 100
-#define MAX 100
+#define MAX 99
 #define THREE 3
 
 //input files -- you might need to change the names depending on what test files you're using 
-const char FULL_FILE_PATH1[LENGTH_FILE_PATH] = "C:\\GithubRepos\\CS2060\\CS-2300---Olivia-Bach\\Program 3\\test_input_1.txt";
-const char FULL_FILE_PATH2[LENGTH_FILE_PATH] = "C:\\GithubRepos\\CS2060\\CS-2300---Olivia-Bach\\Program 3\\test_input_2.txt";
-const char FULL_FILE_PATH3[LENGTH_FILE_PATH] = "C:\\GithubRepos\\CS2060\\CS-2300---Olivia-Bach\\Program 3\\3D_test_input_1.txt";
+const char FULL_FILE_PATH1[LENGTH_FILE_PATH] = "C:\\GithubRepos\\CS2060\\CS-2300---Olivia-Bach\\Program 4\\test_file.txt";
+const char FULL_FILE_PATH2[LENGTH_FILE_PATH] = "C:\\GithubRepos\\CS2060\\CS-2300---Olivia-Bach\\Program 4\\test_input_2.txt";
+const char FULL_FILE_PATH3[LENGTH_FILE_PATH] = "C:\\GithubRepos\\CS2060\\CS-2300---Olivia-Bach\\Program 4\\3D_test_input_1.txt";
 
 //output files -- this is where the output is being sent
-const char FILE_PATH[] = "C:\\GithubRepos\\CS2060\\CS-2300---Olivia-Bach\\Program 3\\";
+const char FILE_PATH[] = "C:\\GithubRepos\\CS2060\\CS-2300---Olivia-Bach\\Program 4\\";
 
 void getPoints(double placeholder[MAX], FILE* filePtr, const char path[LENGTH_FILE_PATH]);
 void createDivideArray(double placeholder[MAX]);
-bool fillValues(double point[THREE], double norm[THREE], double dir[THREE], double temp[MAX], int i);
+void fillValues(double point[THREE], double norm[THREE], double dir[THREE], double temp[MAX]);
+void fillPoints(double temp[MAX], int i, double point1[THREE], double point2[THREE], double point3[THREE]);
+bool ifZero(double temp[MAX], int i);
 
 int main(void) {
 
@@ -47,37 +49,55 @@ int main(void) {
     //this one will have to call all the other functions inside it
     createDivideArray(temp);
 
+    for (int i = 0; i < MAX; i++) {
+ 
+        if (i % 9 == 0) {
+            printf("\n");
+        }
+
+        printf("%.2lf  ", temp[i]);
+    }
+
     return 0;
 }
 
-bool fillValues(double point[THREE], double norm[THREE], double dir[THREE], double temp[MAX], int i) {
-    //every three numbers a different array should be assigned
-    if (i % 9 == 0) {
+bool ifZero(double temp[MAX], int i) {
 
-        dir[0] = temp[i - 2];
-        dir[1] = temp[i - 1];
-        dir[2] = temp[i];
-
+    if (temp[i] == 0 && temp[i + 1] == 0 && temp[i + 2] == 0 && temp[i + 3] == 0 && temp[i + 4] == 0 && temp[i + 5] == 0 && temp[i + 6] == 0 && temp[i + 7] == 0 && temp[i + 8] == 0) {
         return true;
-
     }
-    else if (i % 6 == 0) {
+    return false;
+}
 
-        norm[0] = temp[i - 2];
-        norm[1] = temp[i - 1];
-        norm[2] = temp[i];
+void fillPoints(double temp[MAX], int i, double point1[THREE], double point2[THREE], double point3[THREE]) {
 
-    }
-    else if (i % 3 == 0) {
+    point1[0] = temp[i];
+    point1[1] = temp[i+1];
+    point1[2] = temp[i+2];
 
-        point[0] = temp[i - 2];
-        point[1] = temp[i - 1];
-        point[2] = temp[i];
+    point2[0] = temp[i+3];
+    point2[1] = temp[i+4];
+    point2[2] = temp[i+5];
 
-    }
-    else {
-        //don't do anything
-    }
+    point3[0] = temp[i+6];
+    point3[1] = temp[i+7];
+    point3[2] = temp[i+8];
+}
+
+void fillValues(double point[THREE], double norm[THREE], double dir[THREE], double temp[MAX]) {
+    //every three numbers a different array should be assigned
+   
+        point[0] = temp[0];
+        point[1] = temp[1];
+        point[2] = temp[2];
+
+        norm[0] = temp[3];
+        norm[1] = temp[4];
+        norm[2] = temp[5];
+
+        dir[0] = temp[6];
+        dir[1] = temp[7];
+        dir[2] = temp[8];
 }
 
 void createDivideArray(double placeholder[MAX]) {
@@ -87,43 +107,36 @@ void createDivideArray(double placeholder[MAX]) {
     double direction1[THREE] = { 0 };
 
     double point2[THREE] = { 0 };
-    double normal2[THREE] = { 0 };
-    double direction2[THREE] = { 0 };
-
     double point3[THREE] = { 0 };
-    double normal3[THREE] = { 0 };
-    double direction3[THREE] = { 0 };
-
     double point4[THREE] = { 0 };
-    double normal4[THREE] = { 0 };
-    double direction4[THREE] = { 0 };
-
-    bool full1 = false;
-    bool full2 = false;
-    bool full3 = false;
-    bool full4 = false;
 
     //cant dynamically assign the size of the array so work around that
 
-    for (int i = 0; i < MAX; i++) {
+    fillValues(point1, normal1, direction1, placeholder);
 
-        while (full1 == false) {
-            full1 = fillValues(point1, normal1, direction1, placeholder, i);
+   for (int i = 9; i < MAX; i += 9) {
+        
+        if (!ifZero(placeholder, i)) {
+
+            fillPoints(placeholder, i, point2, point3, point4);
+            //calculate function
+
+            for (int j = 0; j < 3; j++) {
+                printf("\nPoint 2 = %.2lf  -  Point 3 = %.2lf  -  Point 4 = %.2lf", point2[j], point3[j], point4[j]);
+            }
+
+            printf("\n----------------------------------------------\n");
+
+        }
+        else {
+
         }
 
-        while (full2 == false) {
-            full2 = fillValues(point2, normal2, direction2, placeholder, i);
-        }
-
-        while (full3 == false) {
-            full3 = fillValues(point3, normal3, direction3, placeholder, i);
-        }
-
-        while (full4 == false) {
-            full4 = fillValues(point4, normal4, direction4, placeholder, i);
-        }
     }
 
+    for (int k = 0; k < 3; k++) {
+        printf("\nPoint 1 = %.2lf  -  Normal 1 = %.2lf  -  Direction 1 = %.2lf", point1[k], normal1[k], direction1[k]);
+    }
 }
 
 //based off of getMatrix function from PA2 and PA3
